35a0ae5af6719a3b39d898eff8c31d783117735b
abdellah <abdellahedaoudi80@gmail.com>
2024-11-22 19:06:38 +0100

    How to Hash Password and How Use bcrypt

--- src/users/interfaces/users.interfaces.ts
+++ src/users/interfaces/users.interfaces.ts
@@ -5,4 +5,5 @@ export interface Users extends Document {
   readonly firstname: string;
   readonly lastname: number;
   readonly email: string;
+  readonly password: string;
 }
--- src/users/services/Auth/ResetPassword/Auth.service.ts
+++ src/users/services/Auth/ResetPassword/Auth.service.ts
@@ -2,6 +2,7 @@ import { Inject, Injectable, NotFoundException} from '@nestjs/common'
 import { Model } from 'mongoose';
 import { SignInDto, SignUpDto } from 'src/users/controllers/Auth/authdto';
 import { Users } from 'src/users/interfaces/users.interfaces';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,19 +10,35 @@ export class AuthService {
         @Inject('USERS_MODEL')
         private usersModel: Model<Users>,
       ) {}
-      // Post
-      async SignIn(body:SignInDto){
-        const {email,password} = body;
-        const user = await this.usersModel.findOne({email,password});
+      // SignIn 
+      async SignIn(body: SignInDto) {
+        const { email, password } = body;
+    
+        // Find the user by email
+        const user = await this.usersModel.findOne({ email });
+        
         if (!user) {
-          throw new NotFoundException()
+            throw new NotFoundException('User not found');
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            throw new NotFoundException('Invalid Password');
         }
         return user;
-      }
-      async SignUp(body:SignUpDto){
+    }
+      // SignUp
+      async SignUp(body: SignUpDto) {
         console.log(body);
         
-        const createuser  = await this.usersModel.create(body);
-        return createuser ; 
-      }
+        const saltOrRounds = 10;
+        const hashedPassword = await bcrypt.hash(body.password, saltOrRounds);
+    
+        // Replace the plain password with the hashed password
+        body.password = hashedPassword;
+        
+        // Create the user with the hashed password
+        const createUser = await this.usersModel.create(body);
+        
+        return createUser;
+    }
 }
