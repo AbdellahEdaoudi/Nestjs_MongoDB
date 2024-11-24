bdf6eb2a60eaab8dbf9be5b31296b69a7bc73628
abdellah <abdellahedaoudi80@gmail.com>
2024-11-24 20:14:26 +0100

    Create a Static Files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,17 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot(),DatabaseModule, UsersModule],
+  imports: [ConfigModule.forRoot(),ServeStaticModule.forRoot({
+    rootPath: join(__dirname, '..', 'client'),
+    renderPath:"/client",
+    serveStaticOptions: {
+      index:false
+    }
+  }),DatabaseModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
